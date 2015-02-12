8bfb2d0f00d5ef28fe49a07e2934d1de94e775c4
Pedro Liska <pedroliska@gmail.com>
2015-02-11 22:19:16 -0700

    extra insight on person class

--- Website/person-module.js
+++ Website/person-module.js
@@ -2,11 +2,22 @@
 personType = { employee: 'asdfsadf', normal: 'oiweruwer' };
 
 Person = function(type, name) {
+
+    var privateVar = 'yeah';
+
+    var privateFn = function(parameters) {
+
+    };
+
+    //constructor code can go here
+    var greeting = type === personType.employee
+        ? '1800contacts this is ' + name + ', how may I help you?'
+        : 'Sup, this is ' + name + ' what dooo you want?';
+
+
     this.answerPhone = function() {
-        if (type === personType.employee) {
-            return '1800contacts this is ' + name + ', how may I help you?';
-        }
-        return 'Sup, this is ' + name + ' what do you want?';
+        return greeting;
     }
+
 }
 
